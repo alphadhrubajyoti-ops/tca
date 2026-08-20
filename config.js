b05ffd54f90ba341fa2b5b88587555b614c3e5e2
alphadhrubajyoti-ops <alpha.dhrubajyoti@gmail.com>
2026-08-20 23:10:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 "use strict";
 
 window.TCA_CONFIG = {
-  SUPABASE_URL: "https://ghgfcqdepmqqplrhncza.supabase.co",
+
+  SUPABASE_URL:
+    "https://ghgfcqdepmqqplrhncza.supabase.co",
 
   SUPABASE_ANON_KEY:
     "sb_publishable_hFouorpxuecQuk_xkXNwkA_rm6sJDVl",
 
   STUDENT_EMAIL_DOMAIN:
     "students.terabyte.academy"
+
 };
